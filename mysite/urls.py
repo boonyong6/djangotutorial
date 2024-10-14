3c7aadf170d1ac50332147d08b86bf21eeffe034
boonyong <boonyong.kang93@gmail.com>
2024-10-14 14:57:57 +0800

    Add the site home page

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from mysite import views
+
 urlpatterns = [
+    path("", views.index, name="index"),
     # * `include()` allows referencing other URLconf.
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
